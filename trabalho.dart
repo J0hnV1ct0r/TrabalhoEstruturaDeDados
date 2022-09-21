bc21e25523f2417304d64b69039a3fc039fc3d2a
Joao Victor <84153487+J0hnV1ct0r@users.noreply.github.com>
2022-09-21 12:53:11 +0000

    refatoração

--- trabalho.dart
+++ trabalho.dart
@@ -15,26 +15,21 @@ void main() {
   print("Informe uma expreção matematica esquisita:");
   final expressao =  stdin.readLineSync();
   List pilha = expressao!.split(" ");
-  var rep = true;
+  
   List<int> pilha2 = [int.parse(pilha[0])];  
-  //print(pilha2);
-  //do{
     if(pilha.length > 1){
       for(int t = 1; t < pilha.length; t++){
        if(pilha[t] == "+" || pilha[t] == "-" || pilha[t] == "*" || pilha[t] == "/"){
          //print(calculos(pilha[t - 2], pilha[t - 1],pilha[t]));
          pilha2[pilha2.length - 1] = calculos(pilha2[(pilha2.length) - 2], pilha2.last,pilha[t]);
          //print(pilha2);
-     }else{
+       }else{
          pilha2.add(int.parse(pilha[t]));
          print(pilha2);
-     }
-        rep = false;
-   }  
-    }//else{
-      //rep = false;
-    //}
-  //}while(rep);
+       }
+       
+     }  
+    }
   print(pilha2);
 }
 
